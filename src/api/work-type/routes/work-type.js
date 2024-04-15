646ee4046df9ed6e93e1bccf5a42711c0ac9fc8f
perpl777 <mllw327@gmail.com>
2024-04-15 12:05:46 +0700

    no comm

---
+++ src/api/work-type/routes/work-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::work-type.work-type');
---
+++ src/api/work-type/services/work-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::work-type.work-type');
